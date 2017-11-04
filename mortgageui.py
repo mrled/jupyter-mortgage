3a4b4c45a771e8153244e49275c1f00bef27bee3
Micah R Ledbetter <me@micahrl.com>
2017-11-04 15:44:20 -0500

    Add DelayedExecutor class
    Replace StreetMapGlobalManager with more generic DelayedExecutor
    
    Unfortunately, this is not working correctly - it keeps re-executing my
    maps output even when the input has not changed
    
    Also moved some generic functions to util:
    - Move html_hbox to util.html_hbox
    - Move boxwidg to util.label_widget

--- mortgageui.py
+++ mortgageui.py
@@ -16,9 +16,7 @@ from mako.template import Template
 
 import mortgage
 import streetmap
-
-
-WIDGET_DESC_WIDTH = '10em'
+import util
 
 
 def dollar(amount):
@@ -33,15 +31,6 @@ def dollar(amount):
     call in <span> tags, Jupyter does something really fucked up to my text
     """
     return '${:,.2f}'.format(amount)
-
-
-def html_hbox(text, style):
-    """Create a styled HBox from a string containing HTML"""
-
-    hbox = ipywidgets.HBox()
-    hbox.children = [ipywidgets.HTML(text)]
-    hbox.box_style = style
-    return hbox
 
 
 def disablecellscroll():
@@ -162,92 +151,41 @@ def wrap_schedule(apryearly, principal, years, overpayment, appreciation):
     display(parentwidg)
 
 
-class StreetMapGlobalManager():
-    """Street map global manager"""
+def get_streetmap(address, google_api_key):
+    """Show a streetmap"""
 
-    progress_widget = None
-    stopevent = None
-    container = None
+    # NOTE: display(HTML(...)) and display(ipywidgets.HTML(...)) intermixed because it lets me
+    # be lazy about styling and have it mostly look ok
 
-    def stop(self):
-        """Stop execution"""
-        if self.progress_widget is not None:
-            self.progress_widget.close()
-        if self.stopevent is not None:
-            self.stopevent.set()
-        if self.container is not None:
-            self.container.close()
+    output = ipywidgets.Output()
+    with output:
 
-    def reset(self, progwidg):
-        """Stop and reset"""
-        self.stop()
-        self.stopevent = threading.Event()
-        self.progress_widget = progwidg
-        self.container = ipywidgets.VBox()
-        display(self.container)
-        self.container.children = [self.progress_widget]
+        display(HTML(f"<h2>Map & property information</h2>"))
 
-_STREET_MAP_GLOBALS = StreetMapGlobalManager()
+        if google_api_key != "":
+            gmaps.configure(api_key=google_api_key)
+            geocodes = streetmap.geocode_google(address, google_api_key)
+            display(util.html_hbox("Google API key found - using Google maps", "info"))
+        else:
+            geocodes = streetmap.geocode_nominatim(address)
+            display(util.html_hbox("Using OpenStreetMap for map data", "info"))
 
-
-def wrap_streetmap(address, google_api_key, timerlength=2.0):
-    """Show a streetmap after a timer has elapsed
-
-    Every time this function is called, a global timer is started.
-    If this function is called again before the timer elapses, the timer is canceled and restarted.
-    When the timer does finally elapse, the street map is shown.
-
-    address         location to display on the map
-    google_api_key  a valid Google API key, or None
-                    if the API key is valid, use Google maps; otherwise, use OpenStreetMaps
-    timerlength     number of seconds to wait before the street map is displayed
-    """
-
-    def get_streetmap(address, google_api_key):
-        """Show a streetmap"""
-
-        # WARNING: widgets can NOT be displayed directly from a non-main thread!
-        # Ref: https://github.com/jupyter-widgets/ipywidgets/issues/1790
-        # Instead, we must create a container widget, and update its .children property
-        # to contain the items we wish to display
-        # To render HTML, we could place an ipywidgets.HTML() into the container, but that loses
-        # the normal Jupyter styling for HTML elements - things like tables don't look as nice
-        # We instead we render using IPython.display.display(HTML(...)), but we do so into an
-        # ipywidgets.Output() which we then add to the container widget's .children
-
-        # NOTE: display(HTML(...)) and display(ipywidgets.HTML(...)) intermixed because it lets me
-        # be lazy about styling and have it mostly look ok
-
-        container_children = []
-
-        preface_html_out = ipywidgets.Output()
-        with preface_html_out:
-            display(HTML(f"<h2>Map & property information</h2>"))
-
-            if google_api_key != "":
-                gmaps.configure(api_key=google_api_key)
-                geocodes = streetmap.geocode_google(address, google_api_key)
-                display(html_hbox("Google API key found - using Google maps", "info"))
-            else:
-                geocodes = streetmap.geocode_nominatim(address)
-                display(html_hbox("Using OpenStreetMap for map data", "info"))
-
-            if len(geocodes) < 1:
-                display(html_hbox(f"Could not find property at {address}", "danger"))
-            elif len(geocodes) > 1:
-                display(html_hbox(f"{len(geocodes)} matches returned for {address}; all are displayed below", "warning"))
-
-        container_children += [preface_html_out]
+        if len(geocodes) < 1:
+            display(util.html_hbox(
+                f"Could not find property at {address}",
+                "danger"))
+        elif len(geocodes) > 1:
+            display(util.html_hbox(
+                f"{len(geocodes)} matches returned for {address}; all are displayed below",
+                "warning"))
 
         for idx, geocode in enumerate(geocodes):
 
-            property_header = ipywidgets.Output()
-            with property_header:
-                if len(geocodes) != 1:
-                    display(HTML(f"<h3>Property {idx + 1}</h3>"))
-                else:
-                    display(HTML("<h3>Property information</h3>"))
-                display(HTML(f"<p><em>{geocode.displayname}</em></p>"))
+            if len(geocodes) != 1:
+                display(HTML(f"<h3>Property {idx + 1}</h3>"))
+            else:
+                display(HTML("<h3>Property information</h3>"))
+            display(HTML(f"<p><em>{geocode.displayname}</em></p>"))
 
             property_info = ipywidgets.Box()
             property_info.layout = ipywidgets.Layout(
@@ -282,6 +220,7 @@ def wrap_streetmap(address, google_api_key, timerlength=2.0):
                         ipywidgets.Label("Longitude:"),
                         ipywidgets.Label(str(geocode.coordinates[1]))),
                     layout=prop_info_child_layout))
+            display(property_info)
 
             if google_api_key != "":
                 property_figure = gmaps.figure(center=geocode.coordinates, zoom_level=14)
@@ -289,47 +228,12 @@ def wrap_streetmap(address, google_api_key, timerlength=2.0):
             else:
                 property_figure = ipyleaflet.Map(center=geocode.coordinates, zoom=14)
                 property_figure += ipyleaflet.Marker(location=geocode.coordinates)
+            display(property_figure)
 
-            container_children += [property_header, property_info, property_figure]
+    return output
 
-        return container_children
 
-    def streetmap_thread(
-            address,
-            google_api_key,
-            timerlength,
-            stopevent,
-            progresswidget,
-            container,
-            updateinterval=0.2):
-        """Update the streetmap progress widget"""
-
-        # .wait() returns True when stopevent.set() is called from a different thread;
-        # until then, it will block until updateinterval elapses, at which point it returns False
-        while not stopevent.wait(updateinterval):
-
-            progresswidget.value += updateinterval
-            if progresswidget.value >= timerlength:
-                # If the stop event did not fire, then we can execute the map display function
-                container.children = get_streetmap(address, google_api_key)
-                break
-
-        progresswidget.close()
-
-    global _STREET_MAP_GLOBALS
-    _STREET_MAP_GLOBALS.reset(ipywidgets.FloatProgress(
-        description='Loading map...', value=0.0, min=0.0, max=timerlength))
-
-    progress_thread = threading.Thread(
-        target=streetmap_thread,
-        args=(
-            address,
-            google_api_key,
-            timerlength,
-            _STREET_MAP_GLOBALS.stopevent,
-            _STREET_MAP_GLOBALS.progress_widget,
-            _STREET_MAP_GLOBALS.container))
-    progress_thread.start()
+street_map_executor = util.DelayedExecutor()  # pylint: disable=C0103
 
 
 def propertyinfo():
@@ -347,58 +251,57 @@ def propertyinfo():
         """Gather information about a property"""
         closed = wrap_close(saleprice, loanapr, years, propertytaxes)
         wrap_schedule(loanapr, closed.principal_total, years, overpayment, appreciation)
-        wrap_streetmap(address, google_api_key)
+
+        global street_map_executor  # pylint: disable=W0603,C0103
+        streetmap_container = ipywidgets.Box()
+        display(streetmap_container)
+        street_map_executor.run(
+            streetmap_container,
+            "Loading maps...",
+            get_streetmap,
+            action_args=(address, google_api_key))
 
     display(HTML(Template("templ/instructions.mako").render()))
 
     widgets_box = ipywidgets.Box(layout=ipywidgets.Layout(
         display='flex',
         flex_flow='column',
-        #border='solid 2px',
         align_items='stretch',
         width='70%'))
 
-    def boxwidg(label, container, widget):
-        """Helper function for widget-adding boilerplate"""
-        box = ipywidgets.Box()
-        box.children = [ipywidgets.Label(label), widget]
-        container.children += (box,)
-        box.layout = ipywidgets.Layout(
-            display='flex',
-            flex_flow='row',
-            justify_content='space-between')
-        return widget
-
-    loanapr = boxwidg("APR", widgets_box, ipywidgets.BoundedFloatText(
+    loanapr = util.label_widget("APR", widgets_box, ipywidgets.BoundedFloatText(
         value=3.75,
         min=0.01,
         step=0.25))
-    saleprice = boxwidg("Sale price", widgets_box, ipywidgets.BoundedFloatText(
+    saleprice = util.label_widget("Sale price", widgets_box, ipywidgets.BoundedFloatText(
         value=250_000,
         min=1,
         max=1_000_000_000,
         step=1000))
-    years = boxwidg("Loan term in years", widgets_box, ipywidgets.Dropdown(
+    years = util.label_widget("Loan term in years", widgets_box, ipywidgets.Dropdown(
         options=[15, 20, 25, 30],
         value=30))
-    overpayment = boxwidg("Monthly overpayment amount", widgets_box, ipywidgets.BoundedIntText(
-        value=50,
-        min=0,
-        max=1_000_000,
-        step=5))
-    appreciation = boxwidg("Yearly appreciation", widgets_box, ipywidgets.BoundedFloatText(
-        value=0.5,
-        min=-20.0,
-        max=20.0,
-        step=0.5))
-    propertytaxes = boxwidg("Yearly property taxes", widgets_box, ipywidgets.BoundedIntText(
-        value=5500,
-        min=0,
-        max=1_000_000,
-        step=5))
-    address = boxwidg("Property address", widgets_box, ipywidgets.Text(
+    overpayment = util.label_widget(
+        "Monthly overpayment amount", widgets_box, ipywidgets.BoundedIntText(
+            value=50,
+            min=0,
+            max=1_000_000,
+            step=5))
+    appreciation = util.label_widget(
+        "Yearly appreciation", widgets_box, ipywidgets.BoundedFloatText(
+            value=0.5,
+            min=-20.0,
+            max=20.0,
+            step=0.5))
+    propertytaxes = util.label_widget(
+        "Yearly property taxes", widgets_box, ipywidgets.BoundedIntText(
+            value=5500,
+            min=0,
+            max=1_000_000,
+            step=5))
+    address = util.label_widget("Property address", widgets_box, ipywidgets.Text(
         value="1600 Pennsylvania Ave NW, Washington, DC 20500"))
-    google_api_key = boxwidg("Google API key (optional)", widgets_box, ipywidgets.Text(
+    google_api_key = util.label_widget("Google API key (optional)", widgets_box, ipywidgets.Text(
         value=""))
 
     output = ipywidgets.interactive_output(metawrapper, {
--- util.py
+++ util.py
@@ -3,6 +3,153 @@
 """Utilities for jupyter-mortgage"""
 
 import logging
+import threading
+
+import ipywidgets
+
+
+class DelayedExecutor():
+    """Manage delayed execution
+
+    Display widgets/outputs after a timer has elapsed
+    If .run() is called a second time while the timer is still running,
+    cancel the first .run() call and start a new one
+    """
+
+    def __init__(self):
+        self.container = ipywidgets.VBox()
+        self.stopevent = threading.Event()
+        self.thread = None
+        self.progbar_container = None
+        self.thread_output_container = None
+        self.progresswidget = None
+
+    def run(
+            self,
+            output_container,
+            progress_desc,
+            action,
+            action_args=None,
+            action_kwargs=None,
+            timerlength=2.0,
+            timerinterval=0.2):
+        """Run an action in a thread after a delay and display output to an existing container
+
+        WARNING: widgets can NOT be displayed directly from a non-main thread!
+        Ref: https://github.com/jupyter-widgets/ipywidgets/issues/1790
+        Instead, we must create a container widget, and then within the thread we can
+        update its .children property to contain the items we wish to display
+
+        output_container    an ipywidgets.Box which the caller is responsible for displaying
+        progress_desc       description for the progress widget
+        action              a function to call when the timer elapses
+                            it should NOT display widgets directly,
+                            but should return a list of widgets/outputs
+                            that can be append to output_container.children
+        action_args         a tuple of positional arguments for action
+        action_kwargs       a dict of keyword arguments for action
+        timerlength         length of delay
+        timerinterval       update the progress widget this often
+        """
+
+        self.progresswidget = ipywidgets.FloatProgress(
+            description=progress_desc, value=0.0, min=0.0, max=timerlength)
+
+        action_args = action_args or ()
+        action_kwargs = action_kwargs or {}
+
+        self.stopevent.set()
+        if self.thread is not None:
+            # block until self.thread terminates (for self.timerinterval secs or shorter)
+            self.thread.join()
+        self.stopevent.clear()
+
+        self.progresswidget.value = 0.0
+
+        # Create new containers for our progress bar and thread output,
+        # put those in self.container,
+        # and append self.container to output_container.children,
+        # thereby throwing away existing *_container objects from a previous run()
+        # This lets us overwrite the children of self.container,
+        # without losing any preexisting children in container
+        self.progbar_container = ipywidgets.Box()
+        self.thread_output_container = ipywidgets.Box()
+        self.container.children = (self.progbar_container, self.thread_output_container)
+        output_container.children += (self.container,)
+
+        self.thread_output_container.children = ()
+        self.progbar_container.children = (self.progresswidget,)
+
+        self.thread = threading.Thread(target=self.timer, args=(
+            timerinterval, timerlength, action, action_args, action_kwargs))
+        self.thread.start()
+
+    def timer(self, timerinterval, timerlength, action, action_args, action_kwargs):
+        """Start the timer, run the action, and append the results
+
+        timerlength     length of delay
+        timerinterval   update the progress widget this often
+        action          a function to call when the timer elapses
+        action_args     a tuple containing positional arguments to action
+        action_kwargs   a dict containing keyword arguments to action
+        """
+
+        # .wait() returns True when stopevent.set() is called from a different thread;
+        # until then, it will block until updateinterval elapses, at which point it returns False
+        while not self.stopevent.wait(timerinterval):
+
+            self.progresswidget.value += timerinterval
+            if self.progresswidget.value >= timerlength:
+                # If the stop event did not fire,
+                # then we can execute the map display function
+                self.container.children = (self.thread_output_container,)
+                result = action(*action_args, **action_kwargs)
+                self.thread_output_container.children = (result,)
+
+        self.progbar_container.children = ()
+
+
+def html_hbox(text, style):
+    """Create a styled HBox from a string containing HTML
+
+    text    string containing HTML
+    style   a valid box_style value for the HBox
+            one of information, success, warning, failure
+    """
+
+    hbox = ipywidgets.HBox()
+    hbox.children = [ipywidgets.HTML(text)]
+    hbox.box_style = style
+    return hbox
+
+
+def label_widget(label, container, widget):
+    """Create a non-fixed-width label for a widget
+
+    Unlike the description= argument that can be passed to widgets, the label should expand in size
+    to meet the width of the container
+
+    label       text for a new ipywidgets.Label for the widget
+    container   a pre-created ipywidgets.Box to which the label and widget will be added
+                (NOTE: this function does not display this container)
+                for example:
+    widget      a pre-created widget
+
+    Example:
+        widgets_box = ipywidgets.Box(layout=ipywidgets.Layout(
+            display='flex', flex_flow='column', align_items='stretch', width='70%'))
+        propertytaxes = label_widget(
+            "Yearly property taxes", widgets_box, ipywidgets.BoundedIntText(
+                value=5500, min=0, max=1_000_000, step=5))
+    """
+    box = ipywidgets.Box()
+    box.children = [ipywidgets.Label(label), widget]
+    container.children += (box,)
+    box.layout = ipywidgets.Layout(
+        display='flex',
+        flex_flow='row',
+        justify_content='space-between')
+    return widget
 
 
 def getlogger():
